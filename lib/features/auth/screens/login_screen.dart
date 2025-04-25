d97f8a54ac44e2e32d3a24a94a3b16ef7a0b43a4
TalhaShakel <shakeeltalha637@gmail.com>
2025-04-26 04:33:19 +0500

    first commit

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/core/common/sign_in_button.dart';
+
+import '../../../core/constants/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,9 +9,45 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Image.asset(
+          Constants.logoPath,
+          height: 40,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Skip',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      ),
       body: Column(
-        children: [Container()],
+        children: [
+          const SizedBox(height: 30),
+          const Text(
+            'Dive into anything',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+            ),
+          ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              Constants.loginEmotePath,
+              height: 400,
+            ),
+          ),
+          const SizedBox(height: 20),
+          SignInButton()
+        ],
       ),
     );
   }
